91bfae187fde92ba36b3bcc1b1a191487560efcf
kazuki takahashi <tkhsh.schoen@gmail.com>
2019-05-24 20:02:05 +0900

    add controller and msg

---
+++ sq2_ccv_upper_body_controller/include/sq2_ccv_upper_body_controller/sq2_ccv_upper_body_controller.h
@@ -0,0 +1,146 @@
+/*********************************************************************
+ * Software License Agreement (BSD License)
+ *
+ *  Copyright (c) 2013, PAL Robotics, S.L.
+ *  All rights reserved.
+ *
+ *  Redistribution and use in source and binary forms, with or without
+ *  modification, are permitted provided that the following conditions
+ *  are met:
+ *
+ *   * Redistributions of source code must retain the above copyright
+ *     notice, this list of conditions and the following disclaimer.
+ *   * Redistributions in binary form must reproduce the above
+ *     copyright notice, this list of conditions and the following
+ *     disclaimer in the documentation and/or other materials provided
+ *     with the distribution.
+ *   * Neither the name of the PAL Robotics nor the names of its
+ *     contributors may be used to endorse or promote products derived
+ *     from this software without specific prior written permission.
+ *
+ *  THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ *  "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ *  LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
+ *  FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
+ *  COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
+ *  INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
+ *  BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ *  LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+ *  CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+ *  LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN
+ *  ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ *  POSSIBILITY OF SUCH DAMAGE.
+ *********************************************************************/
+
+#include <control_msgs/JointTrajectoryControllerState.h>
+#include <controller_interface/controller.h>
+#include <controller_interface/multi_interface_controller.h>
+#include <geometry_msgs/TwistStamped.h>
+#include <hardware_interface/joint_command_interface.h>
+#include <memory>
+#include <nav_msgs/Odometry.h>
+#include <pluginlib/class_list_macros.hpp>
+#include <realtime_tools/realtime_buffer.h>
+#include <realtime_tools/realtime_publisher.h>
+#include <tf/tfMessage.h>
+
+#include "sq2_ccv_roll_pitch_msgs/RollPitch.h"
+
+namespace sq2_ccv_upper_body_controller{
+
+  /**
+   * This class makes some assumptions on the model of the robot:
+   *  - the rotation axes of wheels are collinear
+   *  - the wheels are identical in radius
+   * Additional assumptions to not duplicate information readily available in the URDF:
+   *  - the wheels have the same parent frame
+   *  - a wheel collision geometry is a cylinder or sphere in the urdf
+   *  - a wheel joint frame center's vertical projection on the floor must lie within the contact patch
+   */
+  class SQ2CCVUpperBodyController
+      : public controller_interface::Controller<hardware_interface::PositionJointInterface>
+  {
+  public:
+    SQ2CCVUpperBodyController();
+
+    /**
+     * \brief Initialize controller
+     * \param hw            Velocity position 
+     * \param root_nh       Node handle at root namespace
+     * \param controller_nh Node handle inside the controller namespace
+     */
+    bool init(hardware_interface::PositionJointInterface* hw,
+              ros::NodeHandle& root_nh,
+              ros::NodeHandle &controller_nh);
+    /**
+     * \brief Updates controller, i.e. computes the odometry and sets the new velocity commands
+     * \param time   Current time
+     * \param period Time since the last called to update
+     */
+    void update(const ros::Time& time, const ros::Duration& period);
+
+    /**
+     * \brief Starts controller
+     * \param time Current time
+     */
+    void starting(const ros::Time& time);
+
+    /**
+     * \brief Stops controller
+     * \param time Current time
+     */
+    void stopping(const ros::Time& /*time*/);
+
+  private:
+    std::string name_;
+
+    /// Hardware handles:
+	std::vector<hardware_interface::JointHandle> roll_joints_;
+	std::vector<hardware_interface::JointHandle> pitch_joints_;
+
+    ros::Subscriber sub_command_;
+
+	int roll_joints_size;
+	int pitch_joints_size;
+
+	/// params
+	double roll_angle_absolute_value_limit_;
+	double pitch_angle_absolute_value_limit_;
+
+    struct Commands
+    {
+      double roll;
+      double pitch;
+      ros::Time stamp;
+
+      Commands() : roll(0.0), pitch(0.0), stamp(0.0) {}
+    };
+    realtime_tools::RealtimeBuffer<Commands> command_;
+
+  private:
+    /**
+     * \brief Brakes the wheels, i.e. sets the velocity to 0
+     */
+    void brake();
+
+    /**
+     * \brief command callback
+     */
+    void cmdCallback(const sq2_ccv_roll_pitch_msgs::RollPitch& command);
+
+    /**
+     * \brief Get the wheel names from a wheel param
+     * \param [in]  controller_nh Controller node handler
+     * \param [in]  joint_param   Param name
+     * \param [out] joint_names   Vector with the whel names
+     * \return true if the joint_param is available and the joint_names are
+     *        retrieved successfully from the param server; false otherwise
+     */
+    bool getJointNames(ros::NodeHandle& controller_nh,
+                       const std::string& joint_param,
+                       std::vector<std::string>& joint_names);
+
+  };
+
+  PLUGINLIB_EXPORT_CLASS(sq2_ccv_upper_body_controller::SQ2CCVUpperBodyController, controller_interface::ControllerBase);
+} // namespace sq2_ccv_upper_body_controller
---
+++ sq2_ccv_upper_body_controller/src/sq2_ccv_upper_body_controller.cpp
@@ -0,0 +1,283 @@
+/*********************************************************************
+ * Software License Agreement (BSD License)
+ *
+ *  Copyright (c) 2013, PAL Robotics, S.L.
+ *  All rights reserved.
+ *
+ *  Redistribution and use in source and binary forms, with or without
+ *  modification, are permitted provided that the following conditions
+ *  are met:
+ *
+ *   * Redistributions of source code must retain the above copyright
+ *     notice, this list of conditions and the following disclaimer.
+ *   * Redistributions in binary form must reproduce the above
+ *     copyright notice, this list of conditions and the following
+ *     disclaimer in the documentation and/or other materials provided
+ *     with the distribution.
+ *   * Neither the name of the PAL Robotics nor the names of its
+ *     contributors may be used to endorse or promote products derived
+ *     from this software without specific prior written permission.
+ *
+ *  THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ *  "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ *  LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
+ *  FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
+ *  COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
+ *  INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
+ *  BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ *  LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+ *  CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+ *  LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN
+ *  ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ *  POSSIBILITY OF SUCH DAMAGE.
+ *********************************************************************/
+
+#include <cmath>
+#include <sq2_ccv_upper_body_controller/sq2_ccv_upper_body_controller.h>
+#include <tf/transform_datatypes.h>
+#include <urdf/urdfdom_compatibility.h>
+#include <urdf_parser/urdf_parser.h>
+
+static double euclideanOfVectors(const urdf::Vector3& vec1, const urdf::Vector3& vec2)
+{
+  return std::sqrt(std::pow(vec1.x-vec2.x,2) +
+                   std::pow(vec1.y-vec2.y,2) +
+                   std::pow(vec1.z-vec2.z,2));
+}
+
+/*
+* \brief Check that a link exists and has a geometry collision.
+* \param link The link
+* \return true if the link has a collision element with geometry 
+*/
+static bool hasCollisionGeometry(const urdf::LinkConstSharedPtr& link)
+{
+  if (!link)
+  {
+    ROS_ERROR("Link == NULL.");
+    return false;
+  }
+
+  if (!link->collision)
+  {
+    ROS_ERROR_STREAM("Link " << link->name << " does not have collision description. Add collision description for link to urdf.");
+    return false;
+  }
+
+  if (!link->collision->geometry)
+  {
+    ROS_ERROR_STREAM("Link " << link->name << " does not have collision geometry description. Add collision geometry description for link to urdf.");
+    return false;
+  }
+  return true;
+}
+
+/*
+ * \brief Check if the link is modeled as a cylinder
+ * \param link Link
+ * \return true if the link is modeled as a Cylinder; false otherwise
+ */
+static bool isCylinder(const urdf::LinkConstSharedPtr& link)
+{
+  if (!hasCollisionGeometry(link))
+  {
+    return false;
+  }
+
+  if (link->collision->geometry->type != urdf::Geometry::CYLINDER)
+  {
+    ROS_DEBUG_STREAM("Link " << link->name << " does not have cylinder geometry");
+    return false;
+  }
+
+  return true;
+}
+
+/*
+ * \brief Check if the link is modeled as a sphere
+ * \param link Link
+ * \return true if the link is modeled as a Sphere; false otherwise
+ */
+static bool isSphere(const urdf::LinkConstSharedPtr& link)
+{
+  if (!hasCollisionGeometry(link))
+  {
+    return false;
+  }
+
+  if (link->collision->geometry->type != urdf::Geometry::SPHERE)
+  {
+    ROS_DEBUG_STREAM("Link " << link->name << " does not have sphere geometry");
+    return false;
+  }
+
+  return true;
+}
+
+namespace sq2_ccv_upper_body_controller{
+
+  SQ2CCVUpperBodyController::SQ2CCVUpperBodyController()
+    : roll_angle_absolute_value_limit_(M_PI/6.0)
+	, pitch_angle_absolute_value_limit_(M_PI/6.0)
+    , roll_joints_size(0)
+    , pitch_joints_size(0)
+  {
+  }
+
+  bool SQ2CCVUpperBodyController::init(hardware_interface::PositionJointInterface* hw,
+            ros::NodeHandle& root_nh,
+            ros::NodeHandle &controller_nh)
+  {
+    const std::string complete_ns = controller_nh.getNamespace();
+    std::size_t id = complete_ns.find_last_of("/");
+    name_ = complete_ns.substr(id + 1);
+
+    // Get joint names from the parameter server
+	std::vector<std::string> roll_joint_names;
+	std::vector<std::string> pitch_joint_names;
+	getJointNames(controller_nh, "roll_joint", roll_joint_names);
+	getJointNames(controller_nh, "pitch_joint", pitch_joint_names);
+    
+	if(roll_joint_names.size() > 0 && pitch_joint_names.size() > 0){
+        roll_joints_size = roll_joint_names.size();
+        pitch_joints_size = pitch_joint_names.size();
+	}else{
+		ROS_ERROR_STREAM_NAMED(name_, "roll or pitch has no joint");
+		return false;
+	}
+
+	roll_joints_.resize(roll_joints_size);
+	pitch_joints_.resize(pitch_joints_size);
+
+	for(size_t i = 0;i < roll_joints_size;i++){
+        ROS_INFO_STREAM_NAMED(name_,
+                              "Adding roll with joint name: " << roll_joint_names[i]);
+		roll_joints_[i] = hw->getHandle(roll_joint_names[i]);
+	}
+	for(size_t i = 0;i < pitch_joints_size;i++){
+        ROS_INFO_STREAM_NAMED(name_,
+                              "Adding pitch with joint name: " << pitch_joint_names[i]);
+		pitch_joints_[i] = hw->getHandle(pitch_joint_names[i]);
+	}
+
+    sub_command_ = controller_nh.subscribe("roll_pitch", 1, &SQ2CCVUpperBodyController::cmdCallback, this);
+
+    return true;
+  }
+
+  void SQ2CCVUpperBodyController::update(const ros::Time& time, const ros::Duration& period)
+  {
+    Commands curr_cmd = *(command_.readFromRT());
+	double roll_angle = curr_cmd.roll;
+	if(roll_angle > M_PI / 2.0){
+		roll_angle -= M_PI;
+	}else if(roll_angle < -M_PI / 2.0){
+		roll_angle += M_PI;
+	}
+	for(size_t i=0;i<roll_joints_size;i++){
+		roll_joints_[i].setCommand(roll_angle);
+	}
+
+	double pitch_angle = curr_cmd.pitch;
+	if(pitch_angle > M_PI / 2.0){
+		pitch_angle -= M_PI;
+	}else if(pitch_angle < -M_PI / 2.0){
+		pitch_angle += M_PI;
+	}
+	for(size_t i=0;i<pitch_joints_size;i++){
+		pitch_joints_[i].setCommand(pitch_angle);
+	}
+  }
+
+  void SQ2CCVUpperBodyController::starting(const ros::Time& time)
+  {
+    brake();
+  }
+
+  void SQ2CCVUpperBodyController::stopping(const ros::Time& /*time*/)
+  {
+    brake();
+  }
+
+  void SQ2CCVUpperBodyController::brake()
+  {
+	for(size_t i=0;i<roll_joints_size;i++){
+		roll_joints_[i].setCommand(0.0);
+	}
+	for(size_t i=0;i<pitch_joints_size;i++){
+		pitch_joints_[i].setCommand(0.0);
+	}
+  }
+
+  void SQ2CCVUpperBodyController::cmdCallback(const sq2_ccv_roll_pitch_msgs::RollPitch& command)
+  {
+    if (isRunning())
+    {
+      // check that we don't have multiple publishers on the command topic
+	  Commands command_struct_;
+      command_struct_.roll   = command.roll;
+      command_struct_.pitch   = command.pitch;
+      command_struct_.stamp = ros::Time::now();
+      command_.writeFromNonRT (command_struct_);
+    }
+    else
+    {
+      ROS_ERROR_NAMED(name_, "Can't accept new commands. Controller is not running.");
+    }
+  }
+
+  bool SQ2CCVUpperBodyController::getJointNames(ros::NodeHandle& controller_nh,
+                              const std::string& joint_param,
+                              std::vector<std::string>& joint_names)
+  {
+      XmlRpc::XmlRpcValue joint_list;
+      if (!controller_nh.getParam(joint_param, joint_list))
+      {
+        ROS_ERROR_STREAM_NAMED(name_,
+            "Couldn't retrieve Joint param '" << joint_param << "'.");
+        return false;
+      }
+
+      if (joint_list.getType() == XmlRpc::XmlRpcValue::TypeArray)
+      {
+        if (joint_list.size() == 0)
+        {
+          ROS_ERROR_STREAM_NAMED(name_,
+              "Joint param '" << joint_param << "' is an empty list");
+          return false;
+        }
+
+        for (int i = 0; i < joint_list.size(); ++i)
+        {
+          if (joint_list[i].getType() != XmlRpc::XmlRpcValue::TypeString)
+          {
+            ROS_ERROR_STREAM_NAMED(name_,
+                "Joint param '" << joint_param << "' #" << i <<
+                " isn't a string.");
+            return false;
+          }
+        }
+
+        joint_names.resize(joint_list.size());
+        for (int i = 0; i < joint_list.size(); ++i)
+        {
+          joint_names[i] = static_cast<std::string>(joint_list[i]);
+        }
+      }
+      else if (joint_list.getType() == XmlRpc::XmlRpcValue::TypeString)
+      {
+        joint_names.push_back(joint_list);
+      }
+      else
+      {
+        ROS_ERROR_STREAM_NAMED(name_,
+            "Joint param '" << joint_param <<
+            "' is neither a list of strings nor a string.");
+        return false;
+      }
+
+      return true;
+  }
+
+} // namespace sq2_ccv_upper_body_controller
+
